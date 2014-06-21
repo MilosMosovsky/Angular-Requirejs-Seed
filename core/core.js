1f585091c29455c6232f11e0cebc8b208dac1b0d
Milos Mosovsky <milos.mosovsky@gmail.com>
2014-06-21 10:06:12 +0200

    Remove root dir reference

--- core/core.js
+++ core/core.js
@@ -1,8 +1,8 @@
 require.config({
     urlArgs: "bust=b3dc97a",
     paths: {
-        'angular'   : '../bower_components/angular/angular.min',
-        'uiRouter'  : '../bower_components/angular-ui-router/release/angular-ui-router.min'
+        'angular'   : './bower_components/angular/angular.min',
+        'uiRouter'  : './bower_components/angular-ui-router/release/angular-ui-router.min'
     },
 
     shim: {
@@ -15,7 +15,7 @@ require.config({
         }
     },
 
-    baseUrl: '/core'
+    baseUrl: 'core'
 });
 
 
